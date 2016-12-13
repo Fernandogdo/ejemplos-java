30629031856b0e985526b54fa7099a6cf9f99926
Fernandogdo <fernandojaviergdo@gmail.com>
2016-12-13 13:07:23 -0500

    recursividad Fernando Leon, Nelson Aranda

---
+++ Trabajo en clase/Recursividad/src/Uno/Ejecutor.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Uno;
+
+/**
+ *
+ * @author SALAS
+ */
+public class Ejecutor {
+    public static void main(String[] args) {
+        
+        CalculoFactorial calculo_factorial = new CalculoFactorial();
+        
+        calculo_factorial.mostrarFactoriales();
+    }
+}
